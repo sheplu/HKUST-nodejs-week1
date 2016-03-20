30fc24f4d0678ed776277c80b1a715737efd86c5
Sheplu <jean.burellier@gmail.com>
2016-03-21 00:18:41 +0100

    update index for express

--- server-1.js
+++ server-1.js
@@ -1,13 +1,20 @@
-var http = require('http');
+var express = require('express'),
+     http = require('http');
 
 var hostname = 'localhost';
 var port = 3000;
 
-var server = http.createServer(function(req, res){
+var app = express();
+
+app.use(function (req, res, next) {
   console.log(req.headers);
     res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('<html><body><h1>Hello World</h1></body></html>');
-  })
+
+});
+
+var server = http.createServer(app);
+
 server.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
 });
